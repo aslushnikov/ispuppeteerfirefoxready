50df43d20a43a682e89f2a7627d4a662b86731ad
Andrey Lushnikov <lushnikov@chromium.org>
2019-02-06 00:09:26 -0800

    chore: create basic cronjob to poll for repo updates

---
+++ cronjob.sh
@@ -0,0 +1,24 @@
+set -e
+
+if [ -e ./puppeteer ]; then
+  cd puppeteer
+  git pull origin master
+else
+  git clone --depth 1 https://github.com/GoogleChrome/puppeteer
+  cd puppeteer
+fi
+
+npm i
+cd experimental/puppeteer-firefox
+npm i
+cd ../..
+
+npm run funit -- --firefox-status | tail -1 > ../status.json
+cd ..
+
+if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+  exit 0;
+fi
+
+git commig -am 'chore: update status.json'
+git push origin master
